5d8b67d1b7c6f9d064c2b1540cf44443a2a81c95
Ralph <Noelralph2006@gmail.com>
2025-11-09 22:21:47 -0500

    extremely small change

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -3,7 +3,7 @@
     <section className="mt-8">
     <h1 className="text-3xl font-bold">Projects</h1>
     <p className="mt-4 text-sm text-neutral-400">
-        Projects will be here but TBA for now.
+        Projects will be here but TBD for now.
     </p>
     </section>
     );
