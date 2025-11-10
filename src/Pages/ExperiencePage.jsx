20741d4e859b964b0fc5c04191899dc6a7f7fbba
Ralph <Noelralph2006@gmail.com>
2025-11-10 13:50:53 -0500

    making a local save just incase i break something

--- src/Pages/ExperiencePage.jsx
+++ src/Pages/ExperiencePage.jsx
@@ -3,7 +3,7 @@
     <section className="mt-8">
     <h1 className="text-3xl font-bold">Experience</h1>
     <p className="mt-4 text-sm text-neutral-400">
-        This is the section where my professional roles and leadership will live.
+        This is the section where my professional roles and leadership will live..
     </p>
     </section>
     );
