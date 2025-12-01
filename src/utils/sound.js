fe8b88444edeb483d07a568e87597a32e430c0ef
Ralph <Noelralph2006@gmail.com>
2025-11-30 23:24:04 -0500

    acc nvm

--- src/utils/sound.js
+++ src/utils/sound.js
@@ -1,46 +1,10 @@
-import { SFX } from "@/utils/brainrot";
-
-let unlocked = false;
-const waiters = [];
-
-
 export function installAudioUnlock() {
-    if (typeof window === "undefined" || unlocked) return;
-
-    const unlockNow = async () => {
-    try {
-    const a = new Audio();
-    a.muted = true;
-    a.playsInline = true;
-    await a.play().catch(() => {});
-    a.pause();
-    } catch {}
-    unlocked = true;
-    window.removeEventListener("pointerdown", unlockNow);
-    window.removeEventListener("keydown", unlockNow);
-    while (waiters.length) waiters.shift()?.();
-    document.dispatchEvent(new Event("ggxly:audio-unlocked"));
-};
-
-window.addEventListener("pointerdown", unlockNow, { once: true, passive: true });
-window.addEventListener("keydown", unlockNow, { once: true });
+if (typeof window === "undefined") return;
 }
-
 
 export function unlock() {
-if (unlocked) return Promise.resolve();
-installAudioUnlock();
-return new Promise((resolve) => waiters.push(resolve));
+return Promise.resolve();
 }
 
-export function playSfx(name, { volume = 1 } = {}) {
-    const src = SFX?.[name];
-    if (!src) return;
-    try {
-    const a = new Audio(src);
-    a.dataset.ggxly = "sfx";
-    a.playsInline = true;
-    a.volume = volume;
-    a.play().catch(() => {});
-} catch {}
+export function playSfx(_name) {
 }
